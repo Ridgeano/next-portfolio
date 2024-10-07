f8d0a898ba9a4ffb1fc29f9ea57e282465c98690
Sean Ridgeon <seanridgeon@live.co.uk>
2024-10-07 20:16:15 +0100

    tweaked colours for better visability

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -10,7 +10,7 @@ export default function Hero() {
       <main className="absolute inset-0 flex flex-col items-center justify-center">
         <div className="container mx-auto px-4">
           <div className="relative w-full max-w-4xl mx-auto">
-            <h1 className="text-5xl sm:text-6xl md:text-7xl lg:text-8xl xl:text-9xl font-bold text-gray-800 tracking-tight text-center">
+            <h1 className="text-5xl sm:text-6xl md:text-7xl lg:text-8xl xl:text-9xl font-bold text-zinc-950 tracking-tight text-center">
               <span className="block">full stack web developer</span>
             </h1>
           </div>
