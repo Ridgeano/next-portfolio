1e6491a663daeed8d9948cbfe56de10e1b0ef8c0
Sean Ridgeon <seanridgeon@live.co.uk>
2024-10-08 13:40:00 +0100

    adjust preloader timeout

--- components/Preloader.tsx
+++ components/Preloader.tsx
@@ -65,7 +65,7 @@ export function Preloader({ onLoadingComplete }: PreloaderProps) {
         }
         return prevProgress + 1
       })
-    }, 30)
+    }, 20)
 
     return () => clearInterval(timer)
   }, [])
