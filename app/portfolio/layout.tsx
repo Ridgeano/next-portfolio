6b077b25ce90ce89b02a4b376358c7e2965461dd
Sean Ridgeon <seanridgeon@live.co.uk>
2024-10-18 11:25:23 +0100

    Added layout.tsx to each project page

---
+++ app/portfolio/layout.tsx
@@ -0,0 +1,30 @@
+import type { Metadata } from 'next'
+import { Major_Mono_Display } from 'next/font/google'
+import { SmoothScroll } from '@/components/SmoothScroll'
+import { SpeedInsights } from '@vercel/speed-insights/next';
+
+const majorMonoDisplay = Major_Mono_Display({
+  subsets: ["latin"],
+  weight: "400",
+  variable: '--font-major-mono-display',
+})
+
+export const metadata: Metadata = {
+  title: 'Sean Ridgeon - Software Developer',
+  description: 'A self-motivated, forward-thinking Full Stack Developer with a keen interest in UX/UI',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={`${majorMonoDisplay.variable} font-major-mono antialiased`}>
+        <SmoothScroll>{children}</SmoothScroll>
+        <SpeedInsights />
+      </body>
+    </html>
+  )
+}
