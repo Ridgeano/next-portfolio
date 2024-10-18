49e01a557c721455fe8958a55ed3d9c09bbbe5c6
Sean Ridgeon <seanridgeon@live.co.uk>
2024-10-18 19:27:41 +0100

    vercel build fix

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { useState, useRef, useEffect } from "react"
+import { useState, useEffect } from "react"
 import { motion, AnimatePresence } from "framer-motion"
 import { Preloader } from "@/components/Preloader"
 import Hero from "@/components/Hero"
